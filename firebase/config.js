9bb4c582d42132dc895a84790b66d8a1cf919b42
Ruben <inkmobilecruzverde@gmail.com>
2026-05-05 17:01:50 +0100

    auth añadido

--- firebase/config.js
+++ firebase/config.js
@@ -1,17 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth"; // 🔥 AÑADIDO
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
+  apiKey: "AIzaSyCQUkrs1QJFmbrAQqt_dRLmgHfU3Zp-c2Y",
   authDomain: "ink-mobile-5ee6a.firebaseapp.com",
   projectId: "ink-mobile-5ee6a",
   storageBucket: "ink-mobile-5ee6a.appspot.com",
-  messagingSenderId: "174258192559",
-  appId: "1:174258192559:web:fc82c92d4d3cf5329c945a"
+  messagingSenderId: "74258192559",
+  appId: "1:74258192559:web:fc82c92d43cf5329c945a",
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app); // 🔥 ESTO ES LO IMPORTANTE
